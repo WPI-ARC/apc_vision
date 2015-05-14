6cb1bbd156e0e9d18a9877a11faf235cdc4f1450
wpi-apc <wpi-apc@wpiapc.(none)>
2015-05-14 16:04:23 -0400

    Fixed math errors in camera calibration

--- src/camera_calibration_node.cpp
+++ src/camera_calibration_node.cpp
@@ -48,7 +48,7 @@ protected:
     boost::mutex image_mutex;
     tf::TransformListener listener;
 
-    std::vector<Eigen::Affine3d> transforms;
+    std::vector<Eigen::Affine3d> tool_base_tfs;
     std::vector<std::vector<cv::Point2f> > image_points;
 
     //sensor_msgs::ImageConstPtr lastImage_left;
@@ -61,6 +61,7 @@ protected:
 
 public:
     void take_sample() {
+        {
         // Lock the image
         boost::mutex::scoped_lock lock(image_mutex);
 
@@ -70,19 +71,20 @@ public:
         Eigen::Affine3d tool_base_tf;
 
         // Wait for tool->base transform (2 sec)
-        bool success = listener.waitForTransform(base_frame, tool_frame, stamp, ros::Duration(2.0));
+        bool success = listener.waitForTransform(tool_frame, base_frame, stamp, ros::Duration(2.0));
         // If transform isn't found in that time, give up
         if(!success) {
             ROS_ERROR("Couldn't look up camera to shelf transform!");
             return;
         }
         // Otherwise, get the transform
-        listener.lookupTransform(base_frame, tool_frame, stamp, transform);
+        listener.lookupTransform(tool_frame, base_frame, stamp, transform);
         // Get an eigen transform from the tf one
         tf::transformTFToEigen(transform, tool_base_tf);
 
         if(cv::findChessboardCorners(lastImage_left->image, cv::Size(5, 4), detected_points)) {
-            transforms.push_back(tool_base_tf);
+            cv::circle(lastImage_left->image, detected_points[0], 10, cv::Scalar(0, 255, 0));
+            tool_base_tfs.push_back(tool_base_tf);
             image_points.push_back(detected_points);
 
             cv::drawChessboardCorners(lastImage_left->image, cv::Size(5, 4), detected_points, true);
@@ -90,6 +92,7 @@ public:
         } else {
             ROS_WARN("Could not detect entire checkerboard in this image. Skipping");
             cv::imshow("calibration", lastImage_left->image);
+        }
         }
 
         cv::waitKey(0);
@@ -101,7 +104,7 @@ public:
         float square_width = 0.0245;
         for(int j = 0; j < 4; ++j) {
             for(int i = 0; i < 5; ++i) {
-                points.push_back(cv::Point3f(i*square_width, j*square_width, 0.0));
+                points.push_back(cv::Point3f(i*square_width, -j*square_width, 0.0));
             }
         }
 
@@ -111,6 +114,11 @@ public:
             take_sample();
         }
         */
+        take_sample();
+        take_sample();
+        take_sample();
+        take_sample();
+        take_sample();
         take_sample();
 
         for(int i = 0; i < image_points.size(); ++i) {
@@ -123,7 +131,7 @@ public:
         //std::vector<cv::Mat_<double> > translations;
         std::vector<cv::Mat> rotations;
         std::vector<cv::Mat> translations;
-        std::vector<Eigen::Affine3d> transforms;
+        std::vector<Eigen::Affine3d> cam_checker_tfs;
 
         cv::calibrateCamera(pattern_points, image_points, lastImage_left->image.size(), cameraMatrix, distCoeffs, rotations, translations);
 
@@ -137,14 +145,32 @@ public:
             return;
         }
 
+        Eigen::Affine3d base_checker_tf(Eigen::Translation3d((0.276+0.0245), (0.589-0.0245), 0.004));
+
         for(int i = 0; i < rotations.size(); ++i) {
-            cv::Rodrigues(rotations[i], rotations[i]);
+            cv::Mat_<double> rotMat;
+            cv::Rodrigues(rotations[i], rotMat);
+            rotations[i] = rotMat;
             Eigen::Translation3d translation(translations[i].at<double>(0), translations[i].at<double>(1), translations[i].at<double>(2));
-            Eigen::Map<Eigen::Matrix3d> rotation((double*)rotations[i].data);
-            Eigen::Affine3d transform = rotation*translation;
-            transforms.push_back(transform);
-            std::cout << "Transform: " << transform.matrix() << std::endl;
+            Eigen::Map<Eigen::Matrix<double, 3,3, Eigen::RowMajor> > rotation((double*)rotMat.data);
+            Eigen::Affine3d cam_checker_tf = translation*rotation;
+            Eigen::Affine3d checker_cam_tf = cam_checker_tf.inverse();
+            cam_checker_tfs.push_back(checker_cam_tf);
+            //Eigen::Affine3d tool_camera_tf = checker_cam_tf*base_checker_tf*tool_base_tfs[i];
+            Eigen::Affine3d tool_camera_tf = tool_base_tfs[i]*base_checker_tf*checker_cam_tf;
+            std::cout << (tool_base_tfs[i]*base_checker_tf).matrix() << std::endl;
+            std::cout << std::endl;
+            std::cout << checker_cam_tf.matrix() << std::endl;
+            std::cout << std::endl;
+            std::cout << base_checker_tf.matrix() << std::endl;
+            std::cout << std::endl;
+            std::cout << tool_base_tfs[i].matrix() << std::endl;
+            std::cout << std::endl;
+            std::cout << tool_camera_tf.matrix() << std::endl;
+            std::cout << std::endl;
+            std::cout << std::endl;
         }
+
 
 
         cv::FileStorage fs("camera_calib.yaml", cv::FileStorage::WRITE);
